fb0d4cb5ce8ce5a76f5290b04b83567b71100d65
Fran <franmiceli08@gmail.com>
2026-05-11 22:48:00 -0300

    add comentarios explicativos para explicar relacion entre index y main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// React crea el DOM (document object model) y lo renderiza en el elemento con id 'root' del HTML
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
