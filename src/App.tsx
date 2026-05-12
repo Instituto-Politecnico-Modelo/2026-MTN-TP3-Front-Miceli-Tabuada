0613424cd97482f0297c5c28f605fb24eae4e4d3
Lorenzo <tabuadalorenzo88@gmail.com>
2026-05-12 16:09:39 -0300

    fix comentarios dentro del componente App

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   return (
     //auth privider maneja autenticacion globalmente.
     <AuthProvider>
-      //BrowserRouter maneja las rutas de la aplicacion.
+      {/* BrowserRouter maneja las rutas de la aplicacion. */}
       <BrowserRouter>
-        // layout agarra toda la aplicacion y le da un diseño consistente. todas las paginas estan dentro de layout.
+        {/* layout agarra toda la aplicacion y le da un diseño consistente. todas las paginas estan dentro de layout. */}
         <Layout>
-          // app router, segun la ruta, muestra un componente diferente. es el encargado de mostrar las paginas de la aplicacion.
+          {/* app router, segun la ruta, muestra un componente diferente. es el encargado de mostrar las paginas de la aplicacion */}
           <AppRouter />
         </Layout>
       </BrowserRouter>
